13c2a55cbfdfb5db999cb113270be6cae81e2301
Rudmanmrrod <rudmanmrrod@gmail.com>
2020-03-27 17:34:11 -0400

    Changed router

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-	publicPath: process.env.NODE_ENV === 'production' ? './dist/':'/dist/',
+	publicPath: process.env.NODE_ENV === 'production' ? './dist/':'',
 	indexPath: process.env.NODE_ENV === 'production' ? '../index.html':'index.html',
 }
