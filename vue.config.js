58acb3a17fe50d3cdc03665aab356121eab094ae
Rudmanmrrod <rudmanmrrod@gmail.com>
2020-03-27 17:14:23 -0400

    Test deploy to github

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-	publicPath: process.env.NODE_ENV === 'production' ? './':'/',
+	publicPath: process.env.NODE_ENV === 'production' ? './covid_19/':'/covid_19/',
 	indexPath: process.env.NODE_ENV === 'production' ? '../index.html':'index.html',
 }
